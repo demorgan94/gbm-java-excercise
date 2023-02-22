49462293fe8ab836a5546cb7191fd8920bab3d01
David Morgan <demorgan94@gmail.com>
2023-02-22 14:20:05 -0600

    generate backend springboot project

---
+++ gbm-excercise-backend/src/main/java/mx/dm/gbmexcercisebackend/GbmExcerciseBackendApplication.java
@@ -0,0 +1,13 @@
+package mx.dm.gbmexcercisebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbmExcerciseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbmExcerciseBackendApplication.class, args);
+	}
+
+}
---
+++ gbm-excercise-backend/src/test/java/mx/dm/gbmexcercisebackend/GbmExcerciseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.dm.gbmexcercisebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbmExcerciseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
